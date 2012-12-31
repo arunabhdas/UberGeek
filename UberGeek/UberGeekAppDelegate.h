25a3f6b094fd8e7c5bde002cb64c4c9f6debafe8
Arunabh Das <arunabhdas@gmail.com>
2012-12-31 14:40:11 -0500

    Initial commit

---
+++ UberGeek/UberGeekAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UberGeekAppDelegate.h
+//  UberGeek
+//
+//  Created by  on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UberGeekAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UberGeek/UberGeekViewController.h
@@ -0,0 +1,13 @@
+//
+//  UberGeekViewController.h
+//  UberGeek
+//
+//  Created by  on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UberGeekViewController : UIViewController
+
+@end
